29915840de37db91e4f090251dfc12366e5844f3
Alirio.Lau <alirio.lau@gmail.com>
2020-03-28 15:43:59 +0800

    Initial Commit

---
+++ UMengSDK/UMengSDK.h
@@ -0,0 +1,30 @@
+//
+//                            Open Your Mind!
+//
+//                  .-~~~~~~~~~-._       _.-~~~~~~~~~-.
+//              __.'              ~.   .~              `.__
+//            .'//                  \./                  \\`.
+//          .'//                     |                     \\`.
+//        .'// .-~"""""""~~~~-._     |     _,-~~~~"""""""~-. \\`.
+//      .'//.-"                 `-.  |  .-'                 "-.\\`.
+//    .'//______.============-..   \ | /   ..-============.______\\`.
+//  .'______________________________\|/______________________________`.
+//
+//  UMengSDK.h
+//  UMengSDK
+//
+//  Created by Alirio Lau on 2020/3/28.
+//  Copyright © 2020 com.alirio.lau.www. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UMengSDK.
+FOUNDATION_EXPORT double UMengSDKVersionNumber;
+
+//! Project version string for UMengSDK.
+FOUNDATION_EXPORT const unsigned char UMengSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UMengSDK/PublicHeader.h>
+
+
